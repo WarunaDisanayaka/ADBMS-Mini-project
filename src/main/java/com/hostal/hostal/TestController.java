c580e9c5821a33d9962c30abef572996a8b44bfa
Waruna <warunahello@gmail.com>
2023-10-06 19:42:33 +0530

    Dependancy updated

---
+++ src/main/java/com/hostal/hostal/TestController.java
@@ -0,0 +1,13 @@
+package com.hostal.hostal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hello I am working";
+    }
+}

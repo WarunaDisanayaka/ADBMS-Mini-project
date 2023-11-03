b933115c619e5cd4ac64b7a6ff0d6b9de2c93ed3
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-11-03 10:59:02 +0530

    create controller

--- src/main/java/com/hostal/hostal/service/RoomsService.java
+++ src/main/java/com/hostal/hostal/service/RoomsService.java
@@ -1,4 +1,22 @@
 package com.hostal.hostal.service;
 
-public class RoomsService {
+import com.hostal.hostal.entity.RoomsEntity;
+import com.hostal.hostal.repository.RoomsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomsService{
+
+
+    private final RoomsRepository roomsRepository;
+
+    @Autowired
+    public RoomsService(RoomsRepository roomsRepository) {
+        this.roomsRepository = roomsRepository;
+    }
+
+    public RoomsEntity saveRoom(RoomsEntity room){
+        return roomsRepository.save(room);
+    }
 }

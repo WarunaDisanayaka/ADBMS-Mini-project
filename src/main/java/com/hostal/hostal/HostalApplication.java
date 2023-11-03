ae08a859108844f2ab47faf8f3413cf3c81f7334
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-11-03 15:14:53 +0530

    Added CORS fixing front end url

--- src/main/java/com/hostal/hostal/HostalApplication.java
+++ src/main/java/com/hostal/hostal/HostalApplication.java
@@ -2,8 +2,10 @@ package com.hostal.hostal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000") // Replace with your frontend domain
 public class HostalApplication {
 
 	public static void main(String[] args) {

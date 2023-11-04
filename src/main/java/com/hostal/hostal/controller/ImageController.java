813ec080d3b090ab1620a92890bd447bf6448be7
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-11-04 12:12:18 +0530

    Image controller endpoint added

--- src/main/java/com/hostal/hostal/controller/ImageController.java
+++ src/main/java/com/hostal/hostal/controller/ImageController.java
@@ -1,6 +1,6 @@
 package com.hostal.hostal.controller;
 
-import jakarta.annotation.Resource;
+import javax.annotation.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,27 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 public class ImageController {
 
-    private static final String baseDirectory = "../qrcodes/";
+    private static final String baseDirectory = "qrcodes/";
 
 
     @GetMapping("/images/{imageName:.+}")
-    public ResponseEntity<Resource> serveImage(@PathVariable String imageName) {
+    public ResponseEntity<?> serveImage(@PathVariable String imageName) {
         try {
-            // Construct the full path to the image file in the "qrcodes" directory
             Path imagePath = Paths.get(baseDirectory, imageName);
-            Resource resource = (Resource) new UrlResource(imagePath.toUri());
+            UrlResource resource = new UrlResource(imagePath.toUri());
 
             if (resource.exists() && resource.isReadable()) {
                 HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.IMAGE_JPEG); // Set the appropriate content type
+                headers.setContentType(MediaType.IMAGE_PNG);
 
                 return ResponseEntity.ok()
                         .headers(headers)
@@ -38,7 +37,10 @@ public class ImageController {
                 return ResponseEntity.notFound().build();
             }
         } catch (IOException e) {
+            e.printStackTrace(); // Log the exception stack trace for debugging purposes
             return ResponseEntity.notFound().build();
         }
     }
+
+
 }

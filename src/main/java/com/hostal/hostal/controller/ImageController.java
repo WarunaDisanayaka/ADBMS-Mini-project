81563248363e5f0351114204e08767c594f808bd
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-11-04 11:52:03 +0530

    Added rest controller

--- src/main/java/com/hostal/hostal/controller/ImageController.java
+++ src/main/java/com/hostal/hostal/controller/ImageController.java
@@ -1,4 +1,11 @@
 package com.hostal.hostal.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ImageController {
+
+    private static final String baseDirectory = "../qrcodes/";
+
+
 }

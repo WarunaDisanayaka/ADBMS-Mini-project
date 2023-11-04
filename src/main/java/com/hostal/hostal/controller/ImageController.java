6b96b7fe02eddfd1ee4eb7362bb882edbed2e384
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-11-04 11:54:15 +0530

    Added get mapping to image controller

--- src/main/java/com/hostal/hostal/controller/ImageController.java
+++ src/main/java/com/hostal/hostal/controller/ImageController.java
@@ -1,6 +1,18 @@
 package com.hostal.hostal.controller;
 
+import jakarta.annotation.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class ImageController {
@@ -8,4 +20,25 @@ public class ImageController {
     private static final String baseDirectory = "../qrcodes/";
 
 
+    @GetMapping("/images/{imageName:.+}")
+    public ResponseEntity<Resource> serveImage(@PathVariable String imageName) {
+        try {
+            // Construct the full path to the image file in the "qrcodes" directory
+            Path imagePath = Paths.get(baseDirectory, imageName);
+            Resource resource = (Resource) new UrlResource(imagePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.IMAGE_JPEG); // Set the appropriate content type
+
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }

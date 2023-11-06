a09a32dec2ab9211c07893dd5787342e1a24223b
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-11-06 23:19:14 +0530

    Maintenance controller added

---
+++ src/main/java/com/hostal/hostal/controller/MaintainanceController.java
@@ -0,0 +1,46 @@
+package com.hostal.hostal.controller;
+
+import com.hostal.hostal.entity.MaintainanceEntity;
+import com.hostal.hostal.service.MaintainanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/maintainance")
+public class MaintainanceController {
+
+    private final MaintainanceService maintainanceService;
+
+    @Autowired
+    public MaintainanceController(MaintainanceService maintainanceService) {
+        this.maintainanceService = maintainanceService;
+    }
+
+    @GetMapping
+    public List<MaintainanceEntity> getAllMaintainanceEntities() {
+        return maintainanceService.getAllMaintainanceEntities();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<MaintainanceEntity> getMaintainanceEntityById(@PathVariable int id) {
+        return maintainanceService.getMaintainanceEntityById(id);
+    }
+
+    @PostMapping
+    public MaintainanceEntity createMaintainanceEntity(@RequestBody MaintainanceEntity entity) {
+        return maintainanceService.createMaintainanceEntity(entity);
+    }
+
+    @PutMapping
+    public MaintainanceEntity updateMaintainanceEntity(@RequestBody MaintainanceEntity entity) {
+        return maintainanceService.updateMaintainanceEntity(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMaintainanceEntity(@PathVariable int id) {
+        maintainanceService.deleteMaintainanceEntity(id);
+    }
+}
---
+++ src/main/java/com/hostal/hostal/repository/MaintainanceRepository.java
@@ -0,0 +1,8 @@
+package com.hostal.hostal.repository;
+
+import com.hostal.hostal.entity.MaintainanceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintainanceRepository extends JpaRepository<MaintainanceEntity, Integer> {
+
+}

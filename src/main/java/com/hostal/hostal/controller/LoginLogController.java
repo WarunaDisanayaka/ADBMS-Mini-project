39e08d67a7dff763539a62a5468343bd5f970e59
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-11-07 11:19:59 +0530

    Update LoginLogController.java

--- src/main/java/com/hostal/hostal/controller/LoginLogController.java
+++ src/main/java/com/hostal/hostal/controller/LoginLogController.java
@@ -1,6 +1,7 @@
 package com.hostal.hostal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/login-logs")
 public class LoginLogController {
 
